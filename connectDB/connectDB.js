2d4e25c9bd685e176c97a297f55c99c61a48ec25
Adarsh kumar <kumaradarsh69171@gmail.com>
2025-04-17 19:31:10 +0530

    donee

--- connectDB/connectDB.js
+++ connectDB/connectDB.js
@@ -3,10 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const mongo_url=process.env.db_url;
 const connectDB = async()=>{
-   const connect = await mongoose.connect(mongo_url , {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-   })
+   const connect = await mongoose.connect(mongo_url)
    if(connect){
     console.log('connection successfull...')
    }else{
